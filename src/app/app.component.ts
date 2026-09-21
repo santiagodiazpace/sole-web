bbb60f1851c8bed2f9569f4667afe98fcbea88d2
santiagodiazpace <santiagodiazpace@hotmail.com>
2026-09-21 19:01:25 -0300

    Cambios estéticos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  activeSection = 'inicio';
+
+  setActiveSection(section: string): void {
+    this.activeSection = section;
+  }
 }
